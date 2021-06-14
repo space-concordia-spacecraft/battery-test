c811a69bcce162b453414ebc057036ad1f1877ee
Ruben <ruben.e.sanchez.a@gmail.com>
2021-06-14 02:20:27 -0400

    empty files

---
+++ arduino/main.cpp
@@ -0,0 +1,15 @@
+#define BUZZ 3
+
+#define CELL_A_DIR 9
+#define CELL_A_CHARGE 8
+
+#define CELL_B_DIR 4
+#define CELL_B_CHARGE 5
+
+void setup() {
+
+}
+
+void loop() {
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Ruben on 6/14/2021.
+//
