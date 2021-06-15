c4690f6d25e6a7b7e6e52c24aa0f60608d9ee060
Cristian Milatinov <milatinovcristian@gmail.com>
2021-06-14 23:02:19 -0400

    add serial port for arduino communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
-#include "MainWindow.h"
+#include <iostream>
+
 #include <QApplication>
 
+#include "MainWindow.h"
+#include "SerialPort.h"
+
+const char * port = R"(\\.\COM3)";
+
 int main(int argc, char** argv) {
-    QApplication a(argc, argv);
 
-    MainWindow w;
-    w.show();
+    SerialPort arduino(port);
+    if(arduino.isConnected()){
+        std::cout << "Connection made" << std::endl << std::endl;
+    }
 
-    return QApplication::exec();
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+//
+//    return QApplication::exec();
 }
