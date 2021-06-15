e64cf7f296acc34698c8c873a2aa9f7963efeb0f
Ruben <ruben.e.sanchez.a@gmail.com>
2021-06-15 01:55:10 -0400

    new UI

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,24 @@
 #include "MainWindow.h"
 #include "SerialPort.h"
 
-const char * port = R"(\\.\COM3)";
+const char * port = R"(\\.\COM4)";
 
 int main(int argc, char** argv) {
 
-    SerialPort arduino(port);
-    if(arduino.isConnected()){
-        std::cout << "Connection made" << std::endl << std::endl;
-    }
-
-//    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
+//    SerialPort arduino(port);
+//    if(arduino.isConnected()){
+//        std::cout << "Connection made" << std::endl << std::endl;
+//    }
 //
-//    return QApplication::exec();
+//    while (arduino.isConnected()) {
+//        char buffer[MAX_DATA_LENGTH];
+//        arduino.readSerialPort(buffer, MAX_DATA_LENGTH);
+//        std::cout << buffer << std::endl;
+//    }
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return QApplication::exec();
 }
